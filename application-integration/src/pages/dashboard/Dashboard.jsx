a8df81206844e0890357ee4479f9445515d57b5e
Cem ÖZGELDİ <cemozgeldi@192.168.1.10>
2023-08-22 02:55:29 +0300

    sass added

--- application-integration/src/pages/dashboard/Dashboard.jsx
+++ application-integration/src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,15 @@
+import "./dasboard.scss";
+
 const dashboard = () => {
-  return <div>dashboard page</div>;
+  return (
+    <div className="dash">
+      <div className="box box1">Box1</div>
+      <div className="box box2">Box2</div>
+      <div className="box box3">Box3</div>
+      <div className="box box4">Box4</div>
+      <div className="box box5">Box5</div>
+    </div>
+  );
 };
 
 export default dashboard;
