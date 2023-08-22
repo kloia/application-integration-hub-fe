6f90df3e13f60f1990230937e686aa79a134107f
Cem ÖZGELDİ <cemozgeldi@192.168.1.10>
2023-08-22 13:50:54 +0300

    dependencies added

---
+++ application-integration/src/components/topBox/topBox.jsx
@@ -0,0 +1,30 @@
+import "./topBox.scss";
+
+const TopBox = () => {
+  return (
+    <div className="topbox">
+      <div className="box box1">
+        <div className="inside1">1</div>
+        <div className="inside2">2</div>
+      </div>
+      <div className="box box2">
+        <div className="inside1">1</div>
+        <div className="inside2">2</div>
+      </div>
+      <div className="box box3">
+        <div className="inside1">1</div>
+        <div className="inside2">2</div>
+      </div>
+      <div className="box box4">
+        <div className="inside1">1</div>
+        <div className="inside2">2</div>
+      </div>
+      <div className="box box5">
+        <div className="inside1">1</div>
+        <div className="inside2">2</div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBox;
--- application-integration/src/pages/dashboard/Dashboard.jsx
+++ application-integration/src/pages/dashboard/Dashboard.jsx
@@ -1,8 +1,11 @@
+import TopBox from "../../components/topBox/topBox";
 import "./dashboard.scss";
 const dashboard = () => {
   return (
     <div className="dash">
-      <div className="box box1">Box1</div>
+      <div className="box box1">
+        <TopBox />
+      </div>
       <div className="box box2">Box2</div>
       <div className="box box3">Box3</div>
       <div className="box box4">Box4</div>
