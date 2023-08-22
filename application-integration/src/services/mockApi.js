91612c69dbd2bd2f4964d73e290d414e035637e9
Cem ÖZGELDİ <cemozgeldi@192.168.1.10>
2023-08-23 01:38:00 +0300

    new folders created.

---
+++ application-integration/src/services/mockApi.js
@@ -0,0 +1,12 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '30d'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': 'b9177390b4msh089a3a1fae035d9p126005jsn0d51136db967',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
